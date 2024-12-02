51317a28237a945862597955e23532512a302086
Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>
2024-12-02 23:35:32 +0900

    fix: correct CORS middleware and update API endpoints
    - Updated CORS middleware to allow requests from any origin.
    - Corrected the API endpoints for `/expense_transactions_monthly_analysis/count_of_distinct_months` and `/expense_categories/count_of_distinct_names` by adding a trailing slash.

--- internal/routers/routers.go
+++ internal/routers/routers.go
@@ -8,7 +8,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
@@ -36,13 +36,13 @@ func SetupRouter() *gin.Engine {
 	expenseTransactionMonthlyAnalysisRoutes := r.Group("/expense_transactions_monthly_analysis")
 	{
 		expenseTransactionMonthlyAnalysisRoutes.GET("/", handlers.GetExpenseTransactionsMonthlyAnalysis)
-		expenseTransactionMonthlyAnalysisRoutes.GET("/count_of_distinct_months", handlers.GetExpenseTransactionsMonthlyAnalysisForCountOfDistinctMonths)
+		expenseTransactionMonthlyAnalysisRoutes.GET("/count_of_distinct_months/", handlers.GetExpenseTransactionsMonthlyAnalysisForCountOfDistinctMonths)
 	}
 
 	expenseCategoriesRoutes := r.Group("/expense_categories")
 	{
 		//expenseCategoriesRoutes.GET("/", handlers.GetExpenseCategories)
-		expenseCategoriesRoutes.GET("/count_of_distinct_names", handlers.GetExpenseCategoriesForCountOfDistinctNames)
+		expenseCategoriesRoutes.GET("/count_of_distinct_names/", handlers.GetExpenseCategoriesForCountOfDistinctNames)
 	}
 
 	return r
